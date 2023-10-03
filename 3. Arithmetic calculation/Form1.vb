60e4051ce80cd33c8a5a610d2ed9be9cc9ca4e75
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-03 11:22:30 +0200

    Add project files.

---
+++ 3. Arithmetic calculation/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		MsgBox("2+2= " & 2 + 2)
+		MsgBox("2 -2=" & 2 - 2)
+		MsgBox("2*2 =" & 2 * 2)
+		MsgBox("2 / 2 =" & 2 / 2)
+		MsgBox("5*2-3=" & 5 * 2 - 3)
+
+	End Sub
+End Class
